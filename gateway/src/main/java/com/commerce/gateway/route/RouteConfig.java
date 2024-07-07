be9b9a5a262f453346d4043442264bcd33c31e25
Shinnybest <kim295521@naver.com>
2024-07-07 12:37:15 +0900

    [ISSUE-#010] Logging Filter을 대기열 서버에만 적용

--- gateway/src/main/java/com/commerce/gateway/route/RouteConfig.java
+++ gateway/src/main/java/com/commerce/gateway/route/RouteConfig.java
@@ -1,12 +1,17 @@
 package com.commerce.gateway.route;
 
+import com.commerce.gateway.filter.LoggingFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class RouteConfig {
+
+    private final LoggingFilter loggingFilter;
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
@@ -17,7 +22,12 @@ public class RouteConfig {
                                 .uri("lb://COUPON-API"))
                 .route("queue-for-reserve",
                         r -> r.path("/queue/**")
-                                .filters(f -> f.stripPrefix(1))
+                                .filters(f -> f.stripPrefix(1)
+                                        .filter(loggingFilter.apply(LoggingFilter.Config.builder()
+                                                .baseMessage("QUEUE-FOR-RESERVE")
+                                                .preLogger(true)
+                                                .postLogger(true)
+                                                .build())))
                                 .uri("lb://QUEUE-FOR-RESERVE"))
                 .build();
     }
